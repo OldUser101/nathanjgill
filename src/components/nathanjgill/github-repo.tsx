48d49a8a413fdb7403e031a4194f8c087fbca51a
Nathan Gill <nathan.j.gill@outlook.com>
2025-02-05 13:19:42 +0000

    Added repo carousel functionality

--- src/components/nathanjgill/github-repo.tsx
+++ src/components/nathanjgill/github-repo.tsx
@@ -1,7 +1,5 @@
 "use client"
 
-import React, { useEffect, useState } from "react";
-import axios from 'axios';
 import {
     Card
 } from "@/components/ui/card"
@@ -9,41 +7,11 @@ import { Button } from '@/components/ui/button';
 import { GitFork, Star, Code, AlertTriangle } from "lucide-react";
 
 interface GitHubRepoCardProps {
-  repo: string;
+  repo: any;
 }
 
 export function GitHubRepoCard({ repo }: GitHubRepoCardProps) {
-    const [repoData, setRepoData] = useState<any>(null);
-    const [loading, setLoading] = useState<boolean>(true);
-
-    useEffect(() => {
-        const fetchRepoData = async () => {
-            try {
-            const response = await axios.get(`https://api.github.com/repos/OldUser101/${repo}`);
-            setRepoData(response.data);
-            setLoading(false);
-            } catch (error) {
-            console.error("Error fetching repo data", error);
-            setLoading(false);
-            }
-        };
-
-        fetchRepoData();
-    }, [repo]);
-
-    if (loading) {
-        return (
-            <Card className="p-4 max-w-xs mx-auto animate-pulse">
-                <div className="h-10 bg-gray-200 rounded-md mb-4"></div>
-                <div className="h-6 bg-gray-200 rounded w-3/4 mb-2"></div>
-                <div className="h-4 bg-gray-200 rounded w-1/2 mb-2"></div>
-                <div className="h-4 bg-gray-200 rounded w-1/2 mb-2"></div>
-                <div className="h-4 bg-gray-200 rounded w-1/2 mb-2"></div>
-            </Card>
-        );
-    }
-
-    if (!repoData) {
+    if (!repo) {
         return (
             <Card className="flex justify-center p-4 max-w-xs mx-auto">
                 <div className="flex items-center text-red-500">
@@ -56,27 +24,29 @@ export function GitHubRepoCard({ repo }: GitHubRepoCardProps) {
 
     return (
         <Card className="p-4 max-w-xs mx-auto">
-            <div className="">
-                <a href={repoData.html_url} target="_blank" rel="noopener noreferrer" className="font-semibold text-xl hover:underline">{repoData.full_name}</a>
-                <p className="font-light text-base mt-2">{repoData.description}</p>
-                <div className="grid grid-cols-2 gap-4">
+            <div className="h-full flex flex-col">
+                <div>
+                    <a href={repo.html_url} target="_blank" rel="noopener noreferrer" className="font-semibold text-xl hover:underline">{repo.full_name}</a>
+                    <p className="font-light text-base mt-2">{repo.description}</p>
+                </div>
+                <div className="grid grid-cols-2 gap-4 mt-auto">
                     <div>
                         <p className="flex items-center space-x-2 text-sm text-gray-700 dark:text-gray-400 mt-2">
                             <Code size={16} />
-                            <span>Language: {repoData.language}</span>
+                            <span>Language: {repo.language}</span>
                         </p>
                         <p className="flex items-center space-x-2 text-sm text-gray-700 dark:text-gray-400 mt-2">
                             <Star size={16} />
-                            <span>Stars: {repoData.stargazers_count}</span>
+                            <span>Stars: {repo.stargazers_count}</span>
                         </p>
                         <p className="flex items-center space-x-2 text-sm text-gray-700 dark:text-gray-400 mt-2">
                             <GitFork size={16} />
-                            <span>Forks: {repoData.forks_count}</span>
+                            <span>Forks: {repo.forks_count}</span>
                         </p>
                     </div>
                     <div className="flex justify-end flex-col">
                         <Button>
-                            <a href={repoData.html_url} target="_blank" rel="noopener noreferrer">
+                            <a href={repo.html_url} target="_blank" rel="noopener noreferrer">
                                 View on GitHub
                             </a>
                         </Button>
--- src/components/nathanjgill/repo-carousel.tsx
+++ src/components/nathanjgill/repo-carousel.tsx
@@ -1,23 +1,58 @@
 "use client"
 
-import * as React from "react"
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { GitHubRepoCard } from "@/components/nathanjgill/github-repo";
+import { AlertTriangle } from "lucide-react";
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel"
 
 export function RepoCarousel() {
+    const [repoList, setRepoList] = useState<any>(null);
+    const [loading, setLoading] = useState<boolean>(true);
+
+    useEffect(() => {
+        const fetchRepoList = async () => {
+            try {
+                const response = await axios.get(`https://api.github.com/users/OldUser101/repos?sort=updated`);
+                setRepoList(response.data.slice(0, 10));
+                setLoading(false);
+            } catch (error) {
+                console.error("Error fetching repo list", error);
+                setLoading(false);
+            }
+        };
+
+        fetchRepoList();
+    }, []);
+
+    if (loading) {
+        return (
+            <div className="border-b border-b-neutral-700 p-6 w-full relative select-none animate-pulse">
+                <div className="h-10 bg-gray-200 rounded-md"></div>
+            </div>
+        );
+    }
+
+    if (!repoList) {
+        return (
+            <div className="border-b border-b-neutral-700 p-6 w-full relative select-none flex justify-center">
+                <div className="flex items-center text-red-500">
+                    <AlertTriangle className="mr-2" />
+                    <span>An error occurred.</span>
+                </div>
+            </div>
+        );
+    }
+
     return (
-        <div className="border-b border-b-neutral-700 p-6 w-full relative">
+        <div className="border-b border-b-neutral-700 p-6 w-full relative select-none">
             <Carousel className="w-full">
                 <CarouselContent>
-                    <CarouselItem className="pl-1 md:basis-1/2 lg:basis-1/3">
-                        <GitHubRepoCard repo="HybridApp"/>
-                    </CarouselItem>
-                    <CarouselItem className="pl-1 md:basis-1/2 lg:basis-1/3">
-                        <GitHubRepoCard repo="IELaunch"/>
-                    </CarouselItem>
-                    <CarouselItem className="pl-1 md:basis-1/2 lg:basis-1/3">
-                        <GitHubRepoCard repo="PowerToys"/>
-                    </CarouselItem>
+                    {repoList.map((repo) => (
+                        <CarouselItem key={repo.full_name} className="flex items-stretch md:basis-1/2 lg:basis-1/3">
+                            <GitHubRepoCard repo={repo}/>
+                        </CarouselItem>
+                    ))}
                 </CarouselContent>
                 <CarouselPrevious className="left-0" />
                 <CarouselNext className="right-0" />
