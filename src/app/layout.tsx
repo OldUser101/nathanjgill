a354a4a076b5641b2b63dc278ab12dc3d5c44a7e
Nathan Gill <nathan.j.gill@outlook.com>
2025-02-07 20:13:47 +0000

    Changed Apple touch icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,8 +18,8 @@ export default function RootLayout({
         <link rel="icon" href="favicon-dark.svg" type="image/svg+xml" media="(prefers-color-scheme: dark)"/>
         <link rel="icon" href="favicon-light.ico" type="image/x-icon" media="(prefers-color-scheme: light)"/>
         <link rel="icon" href="favicon-dark.ico" type="image/x-icon" media="(prefers-color-scheme: dark)"/>
-        <link rel="apple-touch-icon" href="apple-touch-icon-light.png" media="(prefers-color-scheme: light)"/>
-        <link rel="apple-touch-icon" href="apple-touch-icon-dark.png" media="(prefers-color-scheme: dark)"/>
+        <link rel="apple-touch-icon" id="apple-touch-icon" href="apple-touch-icon-light.png" media="(prefers-color-scheme: light)"/>
+        <link rel="apple-touch-icon" id="apple-touch-icon" href="apple-touch-icon-dark.png" media="(prefers-color-scheme: dark)"/>
       </head>
       <body>
         <ThemeProvider
