b62663e0061a3c133c7948a2655cd214cfc6ebab
Nathan Gill <nathan.j.gill@outlook.com>
2025-02-25 12:52:18 +0000

    Added better mobile UI for repo list.

--- src/components/nathanjgill/repo-grid.tsx
+++ src/components/nathanjgill/repo-grid.tsx
@@ -51,17 +51,30 @@ export function RepoGrid() {
 
     return (
         <motion.div initial={{opacity: 0, y: 20}} animate={{opacity: 1, y: 0}} transition={{duration: 0.5}} className="group border-b border-b-neutral-700 overflow-x-hidden relative flex pt-6 pb-6 flex-col">
-            <div className="grid lg:grid-cols-3 sm:grid-cols-1 md:grid-cols-2 gap-4 w-full px-4">
-                {Array.from(Array(6), (_, i) => (
-                    loadMoreIndex > i ? (
-                        repoList.slice(i * 6, (i + 1) * 6).map((repo, index) => (
-                            <GitHubRepoCard repo={repo} key={index}/>
-                    ))) : null
-                ))}
-
+            <div className="hidden md:block">
+                <div className="grid lg:grid-cols-3 sm:grid-cols-1 md:grid-cols-2 gap-4 w-full px-4">
+                    {Array.from(Array(4), (_, i) => (
+                        loadMoreIndex > i ? (
+                            repoList.slice(i * 6, (i + 1) * 6).map((repo, index) => (
+                                <GitHubRepoCard repo={repo} key={index}/>
+                        ))) : null
+                    ))}
+                </div>
+            </div>
+            <div className="block md:hidden">
+                <div className="grid lg:grid-cols-3 sm:grid-cols-1 md:grid-cols-2 gap-4 w-full px-4">
+                    {Array.from(Array(5), (_, i) => (
+                        loadMoreIndex > i ? (
+                            repoList.slice(i * 3, (i + 1) * 3).map((repo, index) => (
+                                <GitHubRepoCard repo={repo} key={index}/>
+                        ))) : null
+                    ))}
+                </div>
             </div>
             {loadMoreIndex * 6 < repoList.length ? 
-                <Button className="mx-auto mt-6" onClick={handleLoadMore}>Load More</Button> : null}
+                <Button className="mx-auto mt-6 hidden md:block" onClick={handleLoadMore}>Load More</Button> : null}
+            {loadMoreIndex * 3 < repoList.length ? 
+                <Button className="mx-auto mt-6 block md:hidden" onClick={handleLoadMore}>Load More</Button> : null}
         </motion.div>
     );
 }
