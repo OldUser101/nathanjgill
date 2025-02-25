815f6f96a02ad8f421ea96d3702640d0f340ed5b
Nathan Gill <nathan.j.gill@outlook.com>
2025-02-25 13:44:07 +0000

    Added brightness control to header to improve navbar visibility.

--- src/components/nathanjgill/header.tsx
+++ src/components/nathanjgill/header.tsx
@@ -7,7 +7,7 @@ import { motion } from "framer-motion";
 
 export function HeaderPanel() {
   return (
-    <motion.header initial={{ opacity: 0, y: -20}} animate={{opacity: 1, y: 0}} transition={{duration: 0.5}} className="border-b border-b-neutral-700 h-14 justify-between flex items-center select-none forecolor p-3 backdrop-blur fixed top-0 left-0 w-full z-50">
+    <motion.header initial={{ opacity: 0, y: -20}} animate={{opacity: 1, y: 0}} transition={{duration: 0.5}} className="border-b border-b-neutral-700 h-14 justify-between flex items-center select-none forecolor p-3 backdrop-blur-lg fixed top-0 left-0 w-full z-50 dark:backdrop-brightness-[0.55] backdrop-brightness-[1.25]">
         <div className="h-16 flex items-center space-x-3 forecolor hidden md:flex">
             <IconText/>
             <Navbar/>
