9b3d9ed33d96f0b2daadea041264ca8265ed9ddd
Nathan Gill <nathan.j.gill@outlook.com>
2025-02-25 10:26:08 +0000

    Fixed icon text shifting when switching light/dark mode.

--- src/components/nathanjgill/icon-text.tsx
+++ src/components/nathanjgill/icon-text.tsx
@@ -4,17 +4,17 @@ import Image from "next/image";
 export function IconText() {
     return (            
         <Link href="/" passHref legacyBehavior>
-            <div className="h-16 flex items-center space-x-3 forecolor cursor-pointer">
+            <div className="h-16 flex items-center forecolor cursor-pointer">
                 <Image src="favicon-light.svg"
                         width={32}
                         height={32}
                         alt="Website Logo"
-                        className="block dark:hidden"/>
+                        className="block dark:hidden mr-3"/>
                 <Image src="favicon-dark.svg"
                         width={32}
                         height={32}
                         alt="Website Logo"
-                        className="hidden dark:block"/>
+                        className="hidden dark:block mr-3"/>
                 <h1 className="text-l font-semibold text-black dark:invert">Nathan Gill</h1>
             </div>
         </Link>
