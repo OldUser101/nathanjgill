3de00491a84bced8af46480bf0c22c2722a39004
Nathan Gill <nathan.j.gill@outlook.com>
2025-02-25 10:05:30 +0000

    Added GitHub profile pictures.

--- src/components/nathanjgill/github-repo.tsx
+++ src/components/nathanjgill/github-repo.tsx
@@ -25,32 +25,37 @@ export function GitHubRepoCard({ repo }: GitHubRepoCardProps) {
 
     return (
         <Card className="p-4 flex-1">
-            <div className="h-full flex flex-col">
+            <div className="h-full flex flex-col select-none">
                 <div>
-                    <a href={repo.url} target="_blank" rel="noopener noreferrer" className="font-semibold text-xl hover:underline">{repo.login}/{repo.name}</a>
-                    <p className="font-light text-base mt-2">{repo.description}</p>
-                </div>
-                <div className="grid grid-cols-2 gap-4 mt-auto">
+                    <img alt="GitHub Avatar" src={repo.avatarUrl} width="36" height="36" className="mb-2 rounded-full"/>
                     <div>
-                        <p className="flex items-center space-x-2 text-sm text-gray-700 dark:text-gray-400 mt-2">
-                            <Code size={16} />
-                            <span>Language: {repo.languageName}</span>
-                        </p>
-                        <p className="flex items-center space-x-2 text-sm text-gray-700 dark:text-gray-400 mt-2">
-                            <Star size={16} />
-                            <span>Stars: {repo.stargazerCount}</span>
-                        </p>
-                        <p className="flex items-center space-x-2 text-sm text-gray-700 dark:text-gray-400 mt-2">
-                            <GitFork size={16} />
-                            <span>Forks: {repo.forkCount}</span>
-                        </p>
+                        <a href={repo.url} target="_blank" rel="noopener noreferrer" className="font-semibold text-xl hover:underline">{repo.login}/{repo.name}</a>
+                        <p className="font-light text-base mt-2">{repo.description}</p>
                     </div>
-                    <div className="flex justify-end flex-col">
-                        <Button>
-                            <a href={repo.url} target="_blank" rel="noopener noreferrer">
-                                View on GitHub
-                            </a>
-                        </Button>
+                </div>
+                <div className="mt-auto">
+                    <p className="flex items-center space-x-2 text-sm text-gray-700 dark:text-gray-400 mt-2 justify-self-end">
+                                <Code size={16} />
+                                <span>Language: {repo.languageName}</span>
+                    </p>
+                    <div className="grid grid-cols-2 gap-4 mt-auto">
+                        <div>
+                            <p className="flex items-center space-x-2 text-sm text-gray-700 dark:text-gray-400 mt-2">
+                                <Star size={16} />
+                                <span>Stars: {repo.stargazerCount}</span>
+                            </p>
+                            <p className="flex items-center space-x-2 text-sm text-gray-700 dark:text-gray-400 mt-2">
+                                <GitFork size={16} />
+                                <span>Forks: {repo.forkCount}</span>
+                            </p>
+                        </div>
+                        <div className="flex justify-end flex-col">
+                            <Button>
+                                <a href={repo.url} target="_blank" rel="noopener noreferrer">
+                                    View on GitHub
+                                </a>
+                            </Button>
+                        </div>
                     </div>
                 </div>
             </div>
