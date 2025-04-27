0b15ffab74b360bca515be091dec0c7ba4a340fa
Nathan Gill <nathan.j.gill@outlook.com>
2025-04-27 19:25:51 +0100

    Better branch switcher on mobile and desktop

--- src/components/nathanjgill/branch-switch.tsx
+++ src/components/nathanjgill/branch-switch.tsx
@@ -3,8 +3,11 @@
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
 import { Button } from '../ui/button';
 import axios from 'axios';
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import { GitBranch, Globe, Check } from 'lucide-react';
+import { Drawer, DrawerContent, DrawerDescription, DrawerHeader, DrawerTitle, DrawerTrigger } from '../ui/drawer';
+import { Dialog, DialogHeader } from '../ui/dialog';
+import { DialogContent, DialogDescription, DialogTitle, DialogTrigger } from '../ui/dialog';
 
 interface DeployData {
     status: number,
@@ -19,6 +22,7 @@ interface DeployData {
 export function BranchSwitch() {
     const [branches, setState] = useState<DeployData | null>(null);
     const [loading, setLoading] = useState<boolean>(true);
+    const [open, setOpen] = useState(false)
 
     useEffect(() => {
         const fetchBranches = async () => {
@@ -50,25 +54,67 @@ export function BranchSwitch() {
     }
 
     return (
-        <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-                <Button variant="ghost" size="icon">
-                    <GitBranch className="h-[1.2rem] w-[1.2rem]"/>
-                </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent>
-                {branches.branches.map((branch, index) => (
-                    <DropdownMenuItem className="flex items-center justify-between" key={index}>
-                            <a href={branch.url} className="flex items-center w-full justify-between gap-2">
-                                <div className="flex items-center gap-2">
-                                    {branch.branch === "master" || branch.branch === "main" ? <Globe /> : <GitBranch />}
-                                    <p className="font-mono">{branch.branch}</p>
-                                </div>
-                                {branch.current ? <Check/> : <div className="w-[1.2rem]" />}
-                            </a>
-                    </DropdownMenuItem>
-                ))}
-            </DropdownMenuContent>
-        </DropdownMenu>
+        <div>
+            <Dialog>
+                <DialogTrigger asChild>
+                    <Button variant="ghost" size="icon" className="hidden lg:flex">
+                        <GitBranch className="h-[1.2rem] w-[1.2rem]"/>
+                    </Button>
+                </DialogTrigger>
+                <DialogContent>
+                    <DialogHeader>
+                        <DialogTitle>
+                            Switch Branch
+                        </DialogTitle>
+                        <DialogDescription>
+                            Switch to a different branch of this website.
+                        </DialogDescription>
+                    </DialogHeader>
+                    <div className="flex flex-col gap-4">
+                        {branches.branches.map((branch, index) => (
+                            <Button variant="ghost" className="flex items-center justify-between" key={index}>
+                                    <a href={branch.url} className="flex items-center w-full justify-between gap-2">
+                                        <div className="flex items-center gap-2">
+                                            {branch.branch === "master" || branch.branch === "main" ? <Globe /> : <GitBranch />}
+                                            <p className="font-mono">{branch.branch}</p>
+                                        </div>
+                                        {branch.current || branch.branch === "master"? <Check/> : <div className="w-[1.2rem]" />}
+                                    </a>
+                            </Button>
+                        ))}   
+                    </div>
+                </DialogContent>
+            </Dialog>
+            <Drawer>
+                <DrawerTrigger asChild>
+                    <Button variant="ghost" size="icon" className="lg:hidden">
+                        <GitBranch className="h-[1.2rem] w-[1.2rem]"/>
+                    </Button>
+                </DrawerTrigger>
+                <DrawerContent className="p-4 flex flex-col gap-4">
+                    <DrawerHeader>
+                        <DrawerTitle>
+                            Switch Branch
+                        </DrawerTitle>
+                        <DrawerDescription>
+                            Switch to different branch of this website.
+                        </DrawerDescription>
+                    </DrawerHeader>
+                    <div className="flex flex-col gap-4">
+                        {branches.branches.map((branch, index) => (
+                            <Button variant="ghost" className="flex items-center justify-between" key={index}>
+                                    <a href={branch.url} className="flex items-center w-full justify-between gap-2">
+                                        <div className="flex items-center gap-2">
+                                            {branch.branch === "master" || branch.branch === "main" ? <Globe /> : <GitBranch />}
+                                            <p className="font-mono">{branch.branch}</p>
+                                        </div>
+                                        {branch.current || branch.branch === "master"? <Check/> : <div className="w-[1.2rem]" />}
+                                    </a>
+                            </Button>
+                        ))}   
+                    </div>               
+                </DrawerContent>
+            </Drawer>
+        </div>
     );
 }
