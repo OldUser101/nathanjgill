37fce7fb33a8111aae4e41fbdc499e27645b525e
Nathan Gill <nathan.j.gill@outlook.com>
2025-04-27 15:47:33 +0100

    Added branch indicator and switcher

---
+++ src/app/api/vercel/deploy/route.ts
@@ -0,0 +1,89 @@
+interface DeployData {
+    status: number,
+    branches: Array<{
+        url: string,
+        branch: string,
+        created: number,
+        current: boolean
+    }>
+}
+
+interface Cache {
+    data: DeployData | null;
+    timestamp: number;
+}
+
+const CACHE_DURATION = 5 * 60 * 1000;
+  
+let cache: Cache = {
+    data: null,
+    timestamp: 0,
+};
+
+export async function GET() {
+    const now = Date.now();
+
+    if (cache.data && now - cache.timestamp < CACHE_DURATION) {
+        return Response.json(cache.data);
+    }
+
+    const { VERCEL_DEPLOYMENTS_TOKEN, REPO_ID, GITHUB_TOKEN, VERCEL_GIT_COMMIT_REF } = process.env;
+
+    const branchesRes = await fetch(`https://api.github.com/repos/OldUser101/${REPO_ID}/branches`, {
+        headers: {
+          Authorization: `Bearer ${GITHUB_TOKEN}`,
+          Accept: 'application/vnd.github+json',
+        },
+    });
+
+    if (!branchesRes.ok) {
+        return Response.json({ status: 500 });
+    }
+
+    const branchesData = await branchesRes.json();
+    const existingBranches = new Set(branchesData.map((b: any) => b.name));
+
+    const deployRes = await fetch(`https://api.vercel.com/v6/deployments?projectId=${REPO_ID}`, {
+        headers: {
+            Authorization: `Bearer ${VERCEL_DEPLOYMENTS_TOKEN}`
+        }
+    })
+
+    if (!deployRes.ok) {
+        return Response.json({ status: 500 });
+    }
+
+    const deploymentsData = await deployRes.json();
+    const deployments = deploymentsData.deployments as any[];
+
+    const latestPerBranch: { [branch: string]: { url: string; created: number, current: boolean } } = {};
+
+    for (const d of deployments) {
+        const branch = d.meta?.githubCommitRef;
+        const created = d.created;
+        if (!branch || !existingBranches.has(branch)) continue;
+        if (!latestPerBranch[branch] || created > latestPerBranch[branch].created) {
+            if (branch === "master" || branch === "main") {
+                latestPerBranch[branch] = { url: "https://nathanjgill.uk", created, current: branch === VERCEL_GIT_COMMIT_REF };
+            } else {
+                latestPerBranch[branch] = { url: `https://${d.url}`, created, current: branch === VERCEL_GIT_COMMIT_REF };
+            }
+        }
+    }
+
+    const branchLinks = Object.entries(latestPerBranch).map(([branch, { url, created, current }]) => ({
+        branch,
+        url,
+        created,
+        current
+    }));
+
+    const rData: DeployData = {
+        status: 200,
+        branches: branchLinks
+    }
+
+    cache = { data: rData, timestamp: now };
+
+    return Response.json(rData);
+}
---
+++ src/components/nathanjgill/branch-switch.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
+import { Button } from '../ui/button';
+import axios from 'axios';
+import {useState, useEffect} from 'react';
+import { GitBranch, Globe, Check } from 'lucide-react';
+
+interface DeployData {
+    status: number,
+    branches: Array<{
+        url: string,
+        branch: string,
+        created: number,
+        current: boolean
+    }>
+}
+
+export function BranchSwitch() {
+    const [branches, setState] = useState<DeployData | null>(null);
+    const [loading, setLoading] = useState<boolean>(true);
+
+    useEffect(() => {
+        const fetchBranches = async () => {
+            try {
+                const response = await axios.get("/api/vercel/deploy");
+                setState(response.data);
+                setLoading(false);
+            } catch (error) {
+                console.error("Error fetching branches", error);
+                setLoading(false);
+            }
+        };
+
+        fetchBranches();
+    }, []);
+
+    if (!branches || loading) {
+        return (
+            <></>
+        );
+    }
+
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button variant="ghost" size="icon">
+                    <GitBranch className="h-[1.2rem] w-[1.2rem]"/>
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+                {branches.branches.map((branch, index) => (
+                    <DropdownMenuItem className="flex items-center justify-between" key={index}>
+                            <a href={branch.url} className="flex items-center w-full justify-between gap-2">
+                                <div className="flex items-center gap-2">
+                                    {branch.branch === "master" || branch.branch === "main" ? <Globe /> : <GitBranch />}
+                                    <p className="font-mono">{branch.branch}</p>
+                                </div>
+                                {branch.current ? <Check/> : <div className="w-[1.2rem]" />}
+                            </a>
+                    </DropdownMenuItem>
+                ))}
+            </DropdownMenuContent>
+        </DropdownMenu>
+    );
+}
